0affd10b8583e8709465eac62a70b8dfe934615d
Ravpreet Singh Maini <ravpreetmaini@outlook.com>
2024-11-11 12:19:21 +0530

    Delete desi_kitchen directory

--- desi_kitchen/desiapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=255)
-    contact = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.name
--- desi_kitchen/desiapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import contact_list, contact_create, contact_update, contact_delete
-
-urlpatterns = [
-    path('', contact_list, name='contact_list'),
-    path('new/', contact_create, name='contact_create'),
-    path('edit/<int:id>/', contact_update, name='contact_update'),
-    path('delete/<int:id>/', contact_delete, name='contact_delete'),
-]
